2724673466da0e75ecc41f25311e14f3420cefd3
scorpioborn <97235353+scorpioborn@users.noreply.github.com>
2023-02-21 18:07:12 +0330

    fix: protocol errors number uniqueness

--- x/house/types/errors.go
+++ x/house/types/errors.go
@@ -8,17 +8,17 @@ import (
 
 // x/house module sentinel errors
 var (
-	ErrEmptyDepositorAddr           = sdkerrors.Register(ModuleName, 1001, "empty depositor address")
-	ErrInvalidSportEventUID         = sdkerrors.Register(ModuleName, 1002, "invalid sport event uid")
-	ErrInvalidAmount                = sdkerrors.Register(ModuleName, 1003, "valid amount should be provided")
-	ErrDepositTooSmall              = sdkerrors.Register(ModuleName, 1004, "deposit is not greater than minimum deposit amount")
-	ErrOrderBookDepositProcessing   = sdkerrors.Register(ModuleName, 1005, "internal error in processing deposit in OB")
-	ErrDepositSetting               = sdkerrors.Register(ModuleName, 1006, "internal error in setting deposit")
-	ErrInvalidparticipationIndex    = sdkerrors.Register(ModuleName, 1007, "invalid participant index")
-	ErrInvalidMode                  = sdkerrors.Register(ModuleName, 1008, "invalid withdrawal mode")
-	ErrDepositNotFound              = sdkerrors.Register(ModuleName, 1009, "deposit not found")
-	ErrWithdrawalTooLarge           = sdkerrors.Register(ModuleName, 1010, "withdrawal is more than unused amount")
-	ErrOrderBookLiquidateProcessing = sdkerrors.Register(ModuleName, 1011, "internal error in processing liquidation in OB")
+	ErrEmptyDepositorAddr           = sdkerrors.Register(ModuleName, 5001, "empty depositor address")
+	ErrInvalidSportEventUID         = sdkerrors.Register(ModuleName, 5002, "invalid sport event uid")
+	ErrInvalidAmount                = sdkerrors.Register(ModuleName, 5003, "valid amount should be provided")
+	ErrDepositTooSmall              = sdkerrors.Register(ModuleName, 5004, "deposit is not greater than minimum deposit amount")
+	ErrOrderBookDepositProcessing   = sdkerrors.Register(ModuleName, 5005, "internal error in processing deposit in OB")
+	ErrDepositSetting               = sdkerrors.Register(ModuleName, 5006, "internal error in setting deposit")
+	ErrInvalidparticipationIndex    = sdkerrors.Register(ModuleName, 5007, "invalid participant index")
+	ErrInvalidMode                  = sdkerrors.Register(ModuleName, 5008, "invalid withdrawal mode")
+	ErrDepositNotFound              = sdkerrors.Register(ModuleName, 5009, "deposit not found")
+	ErrWithdrawalTooLarge           = sdkerrors.Register(ModuleName, 5010, "withdrawal is more than unused amount")
+	ErrOrderBookLiquidateProcessing = sdkerrors.Register(ModuleName, 5011, "internal error in processing liquidation in OB")
 )
 
 const (
