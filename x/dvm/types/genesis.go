7681ea016f86319acbec93db9ab1a6e9e146cbb3
scorpioborn <97235353+scorpioborn@users.noreply.github.com>
2023-03-06 14:33:10 +0330

    feat: dvm validate genesis

--- x/dvm/types/genesis.go
+++ x/dvm/types/genesis.go
@@ -1,5 +1,11 @@
 package types
 
+import (
+	"fmt"
+
+	"github.com/spf13/cast"
+)
+
 // DefaultIndex is the default  global index
 const DefaultIndex uint64 = 1
 
@@ -13,5 +19,21 @@ func DefaultGenesis() *GenesisState {
 // Validate performs basic genesis state validation returning an error upon any
 // failure.
 func (gs GenesisState) Validate() error {
+
+	pubkeyChangeProposalCount := len(gs.ActivePubkeysChangeProposals) +
+		len(gs.FinishedPubkeysChangeProposals)
+	if cast.ToUint64(pubkeyChangeProposalCount) != gs.ProposalStats.PubkeysChangeCount {
+		return fmt.Errorf("sum of active and settled public keys change proposal must be equal to statistics %d <> %d",
+			pubkeyChangeProposalCount, gs.ProposalStats.PubkeysChangeCount)
+	}
+
+	for _, active := range gs.ActivePubkeysChangeProposals {
+		for _, finished := range gs.FinishedPubkeysChangeProposals {
+			if active.Id == finished.Proposal.Id {
+				return fmt.Errorf("proposal with id is present in both active and finished list %d", &active.Id)
+			}
+		}
+	}
+
 	return gs.Params.Validate()
 }
