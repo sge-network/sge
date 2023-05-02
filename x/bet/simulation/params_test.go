7252308c9b20c965c06f59d17aaaaf4d2af9e1f6
scorpioborn <97235353+scorpioborn@users.noreply.github.com>
2023-05-02 18:50:38 +0330

    feat: bet params simulation

---
+++ x/bet/simulation/params_test.go
@@ -0,0 +1,36 @@
+package simulation_test
+
+import (
+	//#nosec
+	"math/rand"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/sge-network/sge/x/bet/simulation"
+)
+
+func TestParamChangest(t *testing.T) {
+	s := rand.NewSource(1)
+	//#nosec
+	r := rand.New(s)
+
+	expected := []struct {
+		composedKey string
+		key         string
+		simValue    string
+		subspace    string
+	}{
+		{"bet/MaxBetByUidQueryCount", "MaxBetByUidQueryCount", "\"23\"", "bet"},
+	}
+
+	paramChanges := simulation.ParamChanges(r)
+	require.Len(t, paramChanges, 1)
+
+	for i, p := range paramChanges {
+		require.Equal(t, expected[i].composedKey, p.ComposedKey())
+		require.Equal(t, expected[i].key, p.Key())
+		require.Equal(t, expected[i].simValue, p.SimValue()(r))
+		require.Equal(t, expected[i].subspace, p.Subspace())
+	}
+}
