3fc6a47483291b053c5e6f1c0ac7b3b9d58ae1bc
scorpioborn <97235353+scorpioborn@users.noreply.github.com>
2023-06-15 13:17:16 +0330

    test: kvstore deposits and withdraw

---
+++ x/house/keeper/keeper_test.go
@@ -0,0 +1,30 @@
+package keeper_test
+
+import (
+	"testing"
+	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/google/uuid"
+	simappUtil "github.com/sge-network/sge/testutil/simapp"
+	"github.com/sge-network/sge/x/house/keeper"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	testMarketUID          = uuid.NewString()
+	testParticipationIndex = uint64(1)
+)
+
+func setupKeeper(t testing.TB) (*keeper.KeeperTest, sdk.Context) {
+	_, k, ctx := setupKeeperAndApp(t)
+
+	return k, ctx
+}
+
+func setupKeeperAndApp(t testing.TB) (*simappUtil.TestApp, *keeper.KeeperTest, sdk.Context) {
+	tApp, ctx, err := simappUtil.GetTestObjects()
+	require.NoError(t, err)
+
+	return tApp, &tApp.HouseKeeper, ctx.WithBlockTime(time.Now())
+}
