8dfe72120074562c0cf50774d4d024be306e5954
scorpioborn <97235353+scorpioborn@users.noreply.github.com>
2023-06-16 13:10:20 +0330

    test: message tests for deposit and withdraw

--- x/house/keeper/keeper_test.go
+++ x/house/keeper/keeper_test.go
@@ -9,12 +9,18 @@ import (
 	"github.com/sge-network/sge/testutil/sample"
 	simappUtil "github.com/sge-network/sge/testutil/simapp"
 	"github.com/sge-network/sge/x/house/keeper"
+	markettypes "github.com/sge-network/sge/x/market/types"
 	"github.com/stretchr/testify/require"
 )
 
 var (
 	testMarketUID        = uuid.NewString()
 	testDepositorAddress = sample.AccAddress()
+	testMarketOdds       = []*markettypes.Odds{
+		{UID: uuid.NewString(), Meta: "Odds 1"},
+		{UID: uuid.NewString(), Meta: "Odds 2"},
+		{UID: uuid.NewString(), Meta: "Odds 3"},
+	}
 )
 
 func setupKeeper(t testing.TB) (*keeper.KeeperTest, sdk.Context) {
---
+++ x/house/keeper/msg_server_deposit_test.go
@@ -0,0 +1,161 @@
+package keeper_test
+
+import (
+	"testing"
+	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/golang-jwt/jwt"
+	"github.com/sge-network/sge/testutil/simapp"
+	simappUtil "github.com/sge-network/sge/testutil/simapp"
+	sgetypes "github.com/sge-network/sge/types"
+	"github.com/sge-network/sge/x/house/types"
+	markettypes "github.com/sge-network/sge/x/market/types"
+	"github.com/spf13/cast"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMsgServerDeposit(t *testing.T) {
+	tApp, k, msgk, ctx, wctx := setupMsgServerAndApp(t)
+	creator := simappUtil.TestParamUsers["user1"]
+	depositor := simappUtil.TestParamUsers["user2"]
+	// var err error
+
+	marketItem := markettypes.Market{
+		UID:     testMarketUID,
+		Creator: creator.Address.String(),
+		StartTS: cast.ToUint64(time.Now().Unix()),
+		EndTS:   cast.ToUint64(ctx.BlockTime().Unix()) + 1000,
+		Odds:    testMarketOdds,
+		Status:  markettypes.MarketStatus_MARKET_STATUS_ACTIVE,
+		BetConstraints: &markettypes.MarketBetConstraints{
+			MinAmount: sdk.NewInt(1),
+			BetFee:    sdk.NewInt(1),
+		},
+	}
+
+	tApp.MarketKeeper.SetMarket(ctx, marketItem)
+
+	var oddsUIDs []string
+	for _, v := range marketItem.Odds {
+		oddsUIDs = append(oddsUIDs, v.UID)
+	}
+	err := tApp.OrderbookKeeper.InitiateOrderBook(ctx, marketItem.UID, oddsUIDs)
+	require.NoError(t, err)
+
+	t.Run("min deposit", func(t *testing.T) {
+		inputDeposit := &types.MsgDeposit{
+			Creator: creator.Address.String(),
+			Amount:  sdk.NewInt(1),
+		}
+
+		_, err := msgk.Deposit(wctx, inputDeposit)
+		require.ErrorIs(t, types.ErrDepositTooSmall, err)
+	})
+
+	t.Run("no ticket", func(t *testing.T) {
+		inputDeposit := &types.MsgDeposit{
+			Creator: creator.Address.String(),
+			Amount:  sdk.NewInt(1000),
+		}
+
+		_, err := msgk.Deposit(wctx, inputDeposit)
+		require.ErrorIs(t, types.ErrInTicketVerification, err)
+	})
+
+	t.Run("no authorization found", func(t *testing.T) {
+		testKyc := &sgetypes.KycDataPayload{
+			Approved: true,
+			ID:       creator.Address.String(),
+		}
+		ticketClaim := jwt.MapClaims{
+			"exp":               time.Now().Add(time.Minute * 5).Unix(),
+			"iat":               time.Now().Unix(),
+			"kyc_data":          testKyc,
+			"depositor_address": depositor.Address.String(),
+		}
+		ticket, err := simapp.CreateJwtTicket(ticketClaim)
+		require.Nil(t, err)
+
+		inputDeposit := &types.MsgDeposit{
+			Creator:   creator.Address.String(),
+			MarketUID: testMarketUID,
+			Amount:    sdk.NewInt(1000),
+			Ticket:    ticket,
+		}
+
+		_, err = msgk.Deposit(wctx, inputDeposit)
+		require.ErrorIs(t, types.ErrAuthorizationNotFound, err)
+	})
+
+	t.Run("success without authorization", func(t *testing.T) {
+		testKyc := &sgetypes.KycDataPayload{
+			Approved: true,
+			ID:       depositor.Address.String(),
+		}
+		ticketClaim := jwt.MapClaims{
+			"exp":      time.Now().Add(time.Minute * 5).Unix(),
+			"iat":      time.Now().Unix(),
+			"kyc_data": testKyc,
+		}
+		ticket, err := simapp.CreateJwtTicket(ticketClaim)
+		require.Nil(t, err)
+
+		inputDeposit := &types.MsgDeposit{
+			Creator:   depositor.Address.String(),
+			MarketUID: testMarketUID,
+			Amount:    sdk.NewInt(1000),
+			Ticket:    ticket,
+		}
+
+		depResp, err := msgk.Deposit(wctx, inputDeposit)
+		require.NoError(t, err)
+		rst, found := k.GetDeposit(ctx,
+			depositor.Address.String(),
+			testMarketUID,
+			depResp.ParticipationIndex,
+		)
+		require.True(t, found)
+		require.Equal(t, inputDeposit.Creator, rst.Creator)
+	})
+
+	t.Run("success with authorization", func(t *testing.T) {
+		err := tApp.AuthzKeeper.SaveGrant(ctx,
+			creator.Address,
+			depositor.Address,
+			types.NewDepositAuthorization(sdk.NewInt(1000)),
+			time.Now().Add(5*time.Minute),
+		)
+		require.NoError(t, err)
+
+		testKyc := &sgetypes.KycDataPayload{
+			Approved: true,
+			ID:       depositor.Address.String(),
+		}
+		ticketClaim := jwt.MapClaims{
+			"exp":               time.Now().Add(time.Minute * 5).Unix(),
+			"iat":               time.Now().Unix(),
+			"depositor_address": depositor.Address.String(),
+			"kyc_data":          testKyc,
+		}
+		ticket, err := simapp.CreateJwtTicket(ticketClaim)
+		require.Nil(t, err)
+
+		inputDeposit := &types.MsgDeposit{
+			Creator:   creator.Address.String(),
+			MarketUID: testMarketUID,
+			Amount:    sdk.NewInt(1000),
+			Ticket:    ticket,
+		}
+
+		depResp, err := msgk.Deposit(wctx, inputDeposit)
+		require.NoError(t, err)
+		rst, found := k.GetDeposit(ctx,
+			depositor.Address.String(),
+			testMarketUID,
+			depResp.ParticipationIndex,
+		)
+		require.True(t, found)
+		require.Equal(t, inputDeposit.Creator, rst.Creator)
+	})
+}
