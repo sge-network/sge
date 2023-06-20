35db14be97cdbd0397c75b1ce85014548c7c2965
scorpioborn <97235353+scorpioborn@users.noreply.github.com>
2023-06-20 13:49:16 +0330

    fix: add bet32 validation of depositor address in ticket

--- x/house/types/ticket.go
+++ x/house/types/ticket.go
@@ -1,11 +1,17 @@
 package types
 
 import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 // Validate validates deposit ticket payload.
 func (payload *DepositTicketPayload) Validate(depositor string) error {
+	_, err := sdk.AccAddressFromBech32(depositor)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid depositor address (%s)", err)
+	}
+
 	if !payload.KycData.Validate(depositor) {
 		return sdkerrors.Wrapf(ErrUserKycFailed, "%s", depositor)
 	}
@@ -15,6 +21,11 @@ func (payload *DepositTicketPayload) Validate(depositor string) error {
 
 // Validate validates withdrawal payload.
 func (payload *WithdrawTicketPayload) Validate(depositor string) error {
+	_, err := sdk.AccAddressFromBech32(depositor)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid depositor address (%s)", err)
+	}
+
 	if !payload.KycData.Validate(depositor) {
 		return sdkerrors.Wrapf(ErrUserKycFailed, "%s", depositor)
 	}
