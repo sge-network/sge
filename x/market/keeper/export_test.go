297c680ff2118698910202b7759d82fe5da0239d
scorpioborn <97235353+scorpioborn@users.noreply.github.com>
2023-06-30 16:34:05 +0330

    fix: remove duplication payouad validation

--- x/market/keeper/export_test.go
+++ x/market/keeper/export_test.go
@@ -1,28 +1,5 @@
 package keeper
 
-import (
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/sge-network/sge/x/market/types"
-)
-
 // KeeperTest is a wrapper object for the keeper, It is being used
 // to export unexported methods of the keeper
 type KeeperTest = Keeper
-
-func (k KeeperTest) ValidateMarketResolution(
-	resolutionPayload types.MarketResolutionTicketPayload,
-) error {
-	return resolutionPayload.Validate()
-}
-
-func (k KeeperTest) ValidateMarketAdd(ctx sdk.Context, addPayload types.MarketAddTicketPayload) error {
-	return addPayload.Validate(ctx)
-}
-
-func (k KeeperTest) ValidateMarketUpdate(
-	ctx sdk.Context,
-	updatePayload types.MarketUpdateTicketPayload,
-	currentMarket types.Market,
-) error {
-	return updatePayload.Validate(ctx)
-}
