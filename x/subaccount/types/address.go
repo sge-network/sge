1dd1cab8be19d84ebe5b5d64075c8c244a667d05
unknown <unknown@faulttolerance.net>
2023-07-05 18:40:58 +0200

    use address module for the account

--- x/subaccount/types/address.go
+++ x/subaccount/types/address.go
@@ -1,12 +1,12 @@
 package types
 
 import (
-	"fmt"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/address"
 	"github.com/cosmos/cosmos-sdk/x/auth/types"
 )
 
 // NewAddressFromSubaccount returns an account address for a subaccount
 func NewAddressFromSubaccount(subaccountID uint64) sdk.AccAddress {
-	return types.NewModuleAddress(fmt.Sprintf("%s/%d", ModuleName, subaccountID))
+	return address.Module(types.ModuleName, sdk.Uint64ToBigEndian(subaccountID))
 }
