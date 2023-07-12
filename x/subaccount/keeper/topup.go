e6fc18dd949835ddd4b7d0568624f3daf1291082
unknown <unknown@faulttolerance.net>
2023-07-12 13:18:17 +0200

    include tests for validation of the message

---
+++ x/subaccount/keeper/topup.go
@@ -0,0 +1,11 @@
+package keeper
+
+import (
+	"context"
+	"github.com/sge-network/sge/x/subaccount/types"
+)
+
+func (m msgServer) TopUp(ctx context.Context, msg *types.MsgTopUp) (*types.MsgTopUpResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
