65c255865a9e290dcdbafec0ec7f8717ab7a7883
unknown unknown <unknown@unknown>
2023-07-21 17:48:08 +0200

    add router

--- x/subaccount/keeper/bet.go
+++ x/subaccount/keeper/bet.go
@@ -3,9 +3,29 @@ package keeper
 import (
 	"context"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/gogo/protobuf/proto"
+	bettypes "github.com/sge-network/sge/x/bet/types"
 	"github.com/sge-network/sge/x/subaccount/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (m msgServer) PlaceBet(ctx context.Context, msg *types.MsgPlaceBet) (*types.MsgPlaceBetResponse, error) {
-	panic("implement me")
+	handler := m.keeper.msgRouter.Handler(msg.Msg)
+	if handler == nil {
+		return nil, status.Error(codes.Unavailable, "the message type is not supported by the subaccount router")
+	}
+	sdkResult, err := handler(sdk.UnwrapSDKContext(ctx), msg.Msg)
+	if err != nil {
+		return nil, err
+	}
+
+	resp := &bettypes.MsgPlaceBetResponse{}
+	err = proto.Unmarshal(sdkResult.Data, resp)
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.MsgPlaceBetResponse{Response: resp}, nil
 }
--- x/subaccount/keeper/keeper.go
+++ x/subaccount/keeper/keeper.go
@@ -1,6 +1,7 @@
 package keeper
 
 import (
+	"github.com/cosmos/cosmos-sdk/baseapp"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
@@ -8,18 +9,24 @@ import (
 	"github.com/sge-network/sge/x/subaccount/types"
 )
 
+type MsgRouter interface {
+	Handler(msg sdk.Msg) baseapp.MsgServiceHandler
+}
+
 type Keeper struct {
 	cdc        codec.Codec
 	storeKey   sdk.StoreKey
 	paramstore paramtypes.Subspace
 	bankKeeper bankkeeper.Keeper
+
+	msgRouter MsgRouter
 }
 
-func NewKeeper(cdc codec.Codec, storeKey sdk.StoreKey, ps paramtypes.Subspace) Keeper {
+func NewKeeper(cdc codec.Codec, storeKey sdk.StoreKey, ps paramtypes.Subspace, router MsgRouter) Keeper {
 	// set KeyTable if it is not already set
 	if !ps.HasKeyTable() {
 		ps = ps.WithKeyTable(types.ParamKeyTable())
 	}
 
-	return Keeper{storeKey: storeKey, cdc: cdc, paramstore: ps}
+	return Keeper{storeKey: storeKey, cdc: cdc, paramstore: ps, msgRouter: router}
 }
