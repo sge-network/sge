009dac2181324f25e78e38a671a15435088671b8
unknown unknown <unknown@unknown>
2023-07-24 17:20:11 +0200

    add more hooks logic

--- x/orderbook/keeper/hooks.go
+++ x/orderbook/keeper/hooks.go
@@ -6,6 +6,9 @@ type Hook interface {
 	AfterBettorWin(ctx sdk.Context, bettor sdk.AccAddress, originalAmount, profit sdk.Int)
 	AfterBettorLoss(ctx sdk.Context, bettor sdk.AccAddress, originalAmount sdk.Int)
 	AfterBettorRefund(ctx sdk.Context, bettor sdk.AccAddress, originalAmount, fee sdk.Int)
+	AfterHouseWin(ctx sdk.Context, house sdk.AccAddress, originalAmount, profit sdk.Int, fee *sdk.Int)
+	AfterHouseLoss(ctx sdk.Context, house sdk.AccAddress, originalAmount sdk.Int, lostAmt sdk.Int, fee *sdk.Int)
+	AfterHouseRefund(ctx sdk.Context, house sdk.AccAddress, originalAmount, fee sdk.Int)
 }
 
 func (k *Keeper) RegisterHook(hook Hook) {
