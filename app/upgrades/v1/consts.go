daa26bdfe617919250b0205bab72297bff234837
scorpioborn <97235353+scorpioborn@users.noreply.github.com>
2023-08-21 14:01:38 +0300

    fix: remove wrong comment

--- app/upgrades/v1/consts.go
+++ app/upgrades/v1/consts.go
@@ -9,7 +9,7 @@ import (
 	"github.com/sge-network/sge/app/upgrades"
 )
 
-// UpgradeName defines the on-chain upgrade name for the Osmosis v14 upgrade.
+// UpgradeName defines the on-chain upgrade name for the v1.1.0 upgrade.
 const UpgradeName = "v1.1.0"
 
 var Upgrade = upgrades.Upgrade{
