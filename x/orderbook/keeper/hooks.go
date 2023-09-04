fbc69666378e66f9fdff8c145aca7d0fda89e008
unknown unknown <unknown@unknown>
2023-09-04 12:13:14 +0200

    fmt 3

--- x/orderbook/keeper/hooks.go
+++ x/orderbook/keeper/hooks.go
@@ -1,16 +1,19 @@
 package keeper
 
-import sdk "github.com/cosmos/cosmos-sdk/types"
+import (
+	"cosmossdk.io/math"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
 
 type Hook interface {
-	AfterBettorWin(ctx sdk.Context, bettor sdk.AccAddress, originalAmount, profit sdk.Int)
-	AfterBettorLoss(ctx sdk.Context, bettor sdk.AccAddress, originalAmount sdk.Int)
-	AfterBettorRefund(ctx sdk.Context, bettor sdk.AccAddress, originalAmount, fee sdk.Int)
+	AfterBettorWin(ctx sdk.Context, bettor sdk.AccAddress, originalAmount, profit math.Int)
+	AfterBettorLoss(ctx sdk.Context, bettor sdk.AccAddress, originalAmount math.Int)
+	AfterBettorRefund(ctx sdk.Context, bettor sdk.AccAddress, originalAmount, fee math.Int)
 
-	AfterHouseWin(ctx sdk.Context, house sdk.AccAddress, originalAmount, profit sdk.Int)
-	AfterHouseLoss(ctx sdk.Context, house sdk.AccAddress, originalAmount, lostAmt sdk.Int)
-	AfterHouseRefund(ctx sdk.Context, house sdk.AccAddress, originalAmount sdk.Int)
-	AfterHouseFeeRefund(ctx sdk.Context, house sdk.AccAddress, fee sdk.Int)
+	AfterHouseWin(ctx sdk.Context, house sdk.AccAddress, originalAmount, profit math.Int)
+	AfterHouseLoss(ctx sdk.Context, house sdk.AccAddress, originalAmount, lostAmt math.Int)
+	AfterHouseRefund(ctx sdk.Context, house sdk.AccAddress, originalAmount math.Int)
+	AfterHouseFeeRefund(ctx sdk.Context, house sdk.AccAddress, fee math.Int)
 }
 
 func (k *Keeper) RegisterHook(hook Hook) {
