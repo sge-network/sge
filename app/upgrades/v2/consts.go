300e283c2f48b697ecb3e2e8ec807f930a7e1642
scorpioborn <97235353+scorpioborn@users.noreply.github.com>
2023-11-02 16:34:31 +0300

    docfix: upgrade handler comment

--- app/upgrades/v2/consts.go
+++ app/upgrades/v2/consts.go
@@ -6,7 +6,7 @@ import (
 	"github.com/sge-network/sge/app/upgrades"
 )
 
-// UpgradeName defines the on-chain upgrade name for the v1.1.0 upgrade.
+// UpgradeName defines the on-chain upgrade name for the v1.1.1 upgrade.
 const UpgradeName = "v1.1.1"
 
 var Upgrade = upgrades.Upgrade{
