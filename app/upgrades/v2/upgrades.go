83a970e014af8deabbfc1ec1426c855867fd6874
scorpioborn <97235353+scorpioborn@users.noreply.github.com>
2023-11-13 22:40:37 +0300

    feat: upgrade handler for params

--- app/upgrades/v2/upgrades.go
+++ app/upgrades/v2/upgrades.go
@@ -5,7 +5,9 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 
+	sdkmath "cosmossdk.io/math"
 	"github.com/sge-network/sge/app/keepers"
+	housetypes "github.com/sge-network/sge/x/house/types"
 )
 
 func CreateUpgradeHandler(
@@ -14,6 +16,24 @@ func CreateUpgradeHandler(
 	keepers *keepers.AppKeepers,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		housePS := keepers.GetSubspace(housetypes.ModuleName)
+
+		if !housePS.Has(ctx, []byte("MaxWithdrawalCount")) {
+			var minDeposit sdkmath.Int
+			housePS.Get(ctx, []byte("MinDeposit"), &minDeposit)
+
+			var houseParticipationFee sdk.Dec
+			housePS.Get(ctx, []byte("HouseParticipationFee"), &houseParticipationFee)
+
+			p := housetypes.NewParams(
+				minDeposit,
+				houseParticipationFee,
+				housetypes.DefaultMaxWithdrawalCount,
+			)
+
+			keepers.HouseKeeper.SetParams(ctx, p)
+		}
+
 		return mm.RunMigrations(ctx, configurator, fromVM)
 	}
 }
