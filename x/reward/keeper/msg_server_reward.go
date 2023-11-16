8522cbf8c4e7cbf5dd9a9ac5ed56d1fbd336e326
scorpioborn <97235353+scorpioborn@users.noreply.github.com>
2023-11-16 13:46:21 +0300

    feat: add event for main and subaccount balance and unlock period

--- x/reward/keeper/msg_server_reward.go
+++ x/reward/keeper/msg_server_reward.go
@@ -72,7 +72,7 @@ func (k msgServer) GrantReward(goCtx context.Context, msg *types.MsgGrantReward)
 	k.SetRewardByReceiver(ctx, types.NewRewardByCategory(msg.Uid, recevier.MainAccountAddr, campaign.RewardCategory))
 	k.SetRewardByCampaign(ctx, types.NewRewardByCampaign(msg.Uid, campaign.UID))
 
-	msg.EmitEvent(&ctx, msg.CampaignUid, msg.Uid, campaign.Promoter, recevier)
+	msg.EmitEvent(&ctx, msg.CampaignUid, msg.Uid, campaign.Promoter, *campaign.RewardAmount, recevier)
 
 	return &types.MsgGrantRewardResponse{}, nil
 }
--- x/reward/types/events.go
+++ x/reward/types/events.go
@@ -7,4 +7,7 @@ const (
 	attributeKeyReceiver       = "receiver"
 	attributeKeyRewardUID      = "reward_uid"
 	attributeKeyRewardPromoter = "promoter"
+	attributeKeyMainAmount     = "main_acc_amount"
+	attributeKeySubAmount      = "sub_acc_amount"
+	attributeKeyUnlockPeriod   = "sub_acc_unlock_period"
 )
