8751f18b0b2b36f363ff29a25931f52d1cf2c3f3
3eyedraga <rahul.gayakwad@stlabs.tech>
2023-12-04 19:32:09 +0530

    chore: gofumpted

--- app/upgrades/v3/upgrades.go
+++ app/upgrades/v3/upgrades.go
@@ -1,11 +1,12 @@
 package v3
 
 import (
+	"time"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 	"github.com/sge-network/sge/app/keepers"
-	"time"
 )
 
 const DefaultExpeditedPeriod time.Duration = time.Hour * 24
