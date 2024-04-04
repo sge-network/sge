6e6b60f8e67b181ba895d14398164c7f902e090f
scorpioborn <97235353+scorpioborn@users.noreply.github.com>
2024-04-04 20:49:59 +0300

    test: fix subaccount duplicate reward grant

--- x/reward/keeper/msg_server_reward_test.go
+++ x/reward/keeper/msg_server_reward_test.go
@@ -707,8 +707,8 @@ func TestMsgApplyBetBonus(t *testing.T) {
 	betBonusCampClaims["reward_type"] = types.RewardType_REWARD_TYPE_BET_DISCOUNT
 	betBonusCampClaims["reward_amount_type"] = types.RewardAmountType_REWARD_AMOUNT_TYPE_PERCENTAGE
 	betBonusCampClaims["reward_amount"] = types.RewardAmount{
-		SubaccountAmount: sdkmath.NewInt(10),
-		UnlockPeriod:     uint64(ctx.BlockTime().Add(10 * time.Minute).Unix()),
+		MainAccountAmount: sdkmath.NewInt(10),
+		UnlockPeriod:      0,
 	}
 	betBonusCampClaims["constraints"] = types.CampaignConstraints{
 		MaxBetAmount: sdkmath.NewInt(300),
