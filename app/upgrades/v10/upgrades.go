f3109c0c887ac0fec0660b2b8d299a7a614a342e
scorpioborn <97235353+scorpioborn@users.noreply.github.com>
2024-06-03 11:34:13 +0300

    feat: register upgrade handler v1.7.1

---
+++ app/upgrades/v10/upgrades.go
@@ -0,0 +1,20 @@
+package v10
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+
+	"github.com/sge-network/sge/app/keepers"
+)
+
+//nolint:staticcheck
+func CreateUpgradeHandler(
+	mm *module.Manager,
+	configurator module.Configurator,
+	k *keepers.AppKeepers,
+) upgradetypes.UpgradeHandler {
+	return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		return mm.RunMigrations(ctx, configurator, fromVM)
+	}
+}
