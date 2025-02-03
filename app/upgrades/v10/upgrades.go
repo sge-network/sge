fbda7e04c7a9098588496310484f25412e609b0f
scorpioborn <97235353+scorpioborn@users.noreply.github.com>
2025-02-03 09:28:17 +0300

    feat: upgrade handler with module accounts funds transfer

--- app/upgrades/v10/upgrades.go
+++ app/upgrades/v10/upgrades.go
@@ -1,19 +1,70 @@
 package v10
 
 import (
+	"strings"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 
 	"github.com/sge-network/sge/app/keepers"
+	"github.com/sge-network/sge/app/params"
+	housetypes "github.com/sge-network/sge/x/house/types"
+	orderbooktypes "github.com/sge-network/sge/x/orderbook/types"
+	rewardtypes "github.com/sge-network/sge/x/reward/types"
 )
 
 func CreateUpgradeHandler(
 	mm *module.Manager,
 	configurator module.Configurator,
-	_ *keepers.AppKeepers,
+	k *keepers.AppKeepers,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, _ upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		var recAddress sdk.AccAddress
+		switch strings.ToLower(ctx.ChainID()) {
+		case "sgenet-1":
+			recAddress = sdk.MustAccAddressFromBech32("sge10gtzxh97gvgt6s58w5gagsacrwddhmgzn5ksk6")
+		case "sge-network-4":
+			recAddress = sdk.MustAccAddressFromBech32("sge19dkag3dkzcmjzhctz8ysfws443yq0nyyu0r8c0")
+		case "stage-sgenetwork":
+			recAddress = sdk.MustAccAddressFromBech32("sge1wkzh54s97m4cr70wfq9j7e4u42mkt2a7hak84c")
+		default:
+			return mm.RunMigrations(ctx, configurator, fromVM)
+		}
+
+		orderBookBalance := k.BankKeeper.GetBalance(
+			ctx,
+			k.AccountKeeper.GetModuleAddress(orderbooktypes.OrderBookLiquidityFunder{}.GetModuleAcc()),
+			params.DefaultBondDenom,
+		)
+		k.BankKeeper.SendCoinsFromModuleToAccount(
+			ctx,
+			orderbooktypes.OrderBookLiquidityFunder{}.GetModuleAcc(),
+			recAddress, sdk.NewCoins(orderBookBalance),
+		)
+
+		houseFeeBalance := k.BankKeeper.GetBalance(
+			ctx,
+			k.AccountKeeper.GetModuleAddress(housetypes.HouseFeeCollectorFunder{}.GetModuleAcc()),
+			params.DefaultBondDenom,
+		)
+		k.BankKeeper.SendCoinsFromModuleToAccount(
+			ctx,
+			housetypes.HouseFeeCollectorFunder{}.GetModuleAcc(),
+			recAddress, sdk.NewCoins(houseFeeBalance),
+		)
+
+		rewardPoolBalance := k.BankKeeper.GetBalance(
+			ctx,
+			k.AccountKeeper.GetModuleAddress(rewardtypes.RewardPoolFunder{}.GetModuleAcc()),
+			params.DefaultBondDenom,
+		)
+		k.BankKeeper.SendCoinsFromModuleToAccount(
+			ctx,
+			rewardtypes.RewardPoolFunder{}.GetModuleAcc(),
+			recAddress, sdk.NewCoins(rewardPoolBalance),
+		)
+
 		return mm.RunMigrations(ctx, configurator, fromVM)
 	}
 }
